bfd3a235d2c69ef9712cb12126f3d167da91600f
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-07-17 13:32:28 -0400

    sinatra basic routes lab complete

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get "/name" do
+		"My name is Jesse"
+	end
+
+	get "/hometown" do 
+		"My hometown is Hartford, CT"
+	end
+
+	get "/favorite-song" do 
+		"My favorite song is 'The Next Step' by Kurt Rosenwinkel"
+	end
 end
